7cac104427f5d5b6753a0b3f2cd49d68e4e08914
Ubuntu <ubuntu@ip-172-31-6-37.ap-northeast-1.compute.internal>
2023-02-02 15:08:34 +0000

    kill all containers

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 echo "Starting to deploy docker image.."
 DOCKER_IMAGE=yotamnitzanim/nitzanim-ex-01
 docker pull $DOCKER_IMAGE
+docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker run -d -p 5000:5000 $DOCKER_IMAGE
